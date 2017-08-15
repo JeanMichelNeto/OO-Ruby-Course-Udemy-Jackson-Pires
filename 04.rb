61992e9c48288604843bbf8afd1fa4866c071736
Michel <jeanmichelneto@gmail.com>
2017-08-15 11:13:55 -0300

    Coerção

---
+++ 04.rb
@@ -0,0 +1,8 @@
+
+#to_i - Para converter em Números Inteiros
+#to_s - Para converter em String
+#to_f - Para Float
+puts "Digite Sua Idade!"
+v1 = gets.chomp.to_i
+v2 = v1 + 1
+puts "Sua Idade Ano Que vem Será: #{v2}"
