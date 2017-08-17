72d3a1f87c5cc3d92829d909a48535b1d91706e6
Michel <jeanmichelneto@gmail.com>
2017-08-17 12:19:41 -0300

    new commit

---
+++ 08.rb
@@ -0,0 +1,14 @@
+i = 1
+
+while i <= 50 # testa  enquanto for verdadeiro
+    puts "Michel - #{i}"
+    i += 1
+end
+
+# i == 50
+until i <= 0 # testa enquanto for falso
+puts "Fake! - #{i}"
+i -= 1
+
+end
+puts "Finish!"
